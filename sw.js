b9bf14bdad8709f83ac3b08f630465fc769e9cfe
4ur0n <boomrock46@gmail.com>
2025-12-12 12:31:34 +0800

    Site updated: 2025-12-12 12:31:34

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1764779564630";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1765513893318";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
