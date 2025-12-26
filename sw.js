03a1d71f8901757d52191cb1746970c150872572
4ur0n <boomrock46@gmail.com>
2025-12-27 02:06:04 +0800

    Site updated: 2025-12-27 02:06:04

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1766679825170";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1766772362049";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
